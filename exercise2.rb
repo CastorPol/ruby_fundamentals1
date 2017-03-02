c2117fbb988238896a7817a9306c871609167037
Michael James <michaeljames@nili.ca>
2017-03-02 12:56:03 -0500

    question 4 completed

--- exercise2.rb
+++ exercise2.rb
@@ -11,4 +11,9 @@ number = 26
 
  puts my_string + number.to_s
 
+#question 3, output 45628 * 7839 in a sentence with string interpolation
  puts "the result of 45628 * 7839 is... #{45628*7839}"
+
+ #question 4, whats the value of expression (10 < 20 && 30 < 20) || !(10 ==11)
+
+ #answer evaluates to true
