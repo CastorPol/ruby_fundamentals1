649fa954c8010d57a91591eb0577c5493eb72178
Michael James <michaeljames@nili.ca>
2017-03-02 12:45:40 -0500

    question 2 complete

--- exercise2.rb
+++ exercise2.rb
@@ -1,6 +1,12 @@
-#question 1 calculate tip for $55 meal.
+#question 1, calculate tip for $55 meal.
 def meal(cost)
   puts cost * 0.15
 end
 
 meal(55)
+
+#question 2, add a string and integer with + and convert to string
+my_string = "I was born on May..."
+number = 26
+
+ puts my_string + number.to_s
