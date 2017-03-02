d8f9af655e790c6b4e5b81076aad62d5dfc3515f
Michael James <michaeljames@nili.ca>
2017-03-02 16:37:51 -0500

    question 3 complete

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,17 @@
-puts "What is your name?"
-name = gets.chomp
-puts "hey #{name}!"
+def name
+  puts "What is your name?"
+    name = gets.chomp
+  puts "hey #{name}!"
+end
+
+
+def year_born
+  puts "What year is it?"
+    year = gets.chomp.to_i
+  puts "How old are you?"
+    age = gets.chomp.to_i
+  puts "you were born in #{year - age}"
+end
+
+name
+year_born
